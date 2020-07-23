6cbb14cb1a5306d5e1fb7d0766c5521e68634fdc
changduoduo <1376197813@qq.com>
2020-07-23 12:04:01 +0800

    修改3

--- src/main/java/tryone.java
+++ src/main/java/tryone.java
@@ -2,6 +2,8 @@ public class tryone {
     public static void main(String[] args) {
         System.out.println("aaaaaa");
         System.out.println("bbbbbb");
+        System.out.println("bbbbbb");
+        System.out.println("bbbbbb");
         System.out.println("cccccc");
     }
 }
