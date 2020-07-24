1865b61975c3fec06c866bef0575ed8675e1945c
changduoduo <1376197813@qq.com>
2020-07-24 15:17:05 +0800

    修改测试

--- src/main/java/trytwo.java
+++ src/main/java/trytwo.java
@@ -1,5 +1,7 @@
 public class trytwo {
     public static void main(String[] args) {
         System.out.println("第二个分支");
+        System.out.println("第三个分支");
+        System.out.println("2020_7_24");
     }
 }
